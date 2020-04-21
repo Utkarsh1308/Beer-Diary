515e42fe33d247477b9ae8cf61b4724f08704fba
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2020-04-21 18:03:13 +0530

    Added Templates and Templatetags
    Created templates for the index and Beer details page.
    Created custom_url templatetag
    Modified views functions and urls

--- beers/urls.py
+++ beers/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "beers"
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<slug:beer>', views.beer, name='beer')
 ]
--- beers/views.py
+++ beers/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Beer
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, This is the beers diary")
+    Beers = Beer.objects.all()
+    context = {
+        'Beers': Beers
+    }
+
+    return render(request, 'beers/index.html', context)
+
+def beer(request, beer):
+    def get_beer(beer):
+        if '_' in str(beer):
+            sections = beer.split('_')
+            sections = [section.capitalize() for section in sections]
+            y = " ".join(sections)
+            return y
+        return beer.upper() if len(beer) == 2 else beer.capitalize()
+
+    Beers = Beer.objects.all()
+    section = get_object_or_404(Beer, name=get_beer(beer))
+    context = {
+        'section': section,
+        'Beers': Beers,
+    }
+    return render(request, 'beers/beer.html', context)
