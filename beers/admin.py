ff681527c74133e45f7f9a060757d9283c1a8fe4
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2020-04-21 16:47:53 +0530

    Added Models and Admin Interface

--- beers/admin.py
+++ beers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Beer
 
 # Register your models here.
+class BeerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name',            {'fields': ['name']}),
+        ('Brewer',          {'fields': ['brewer']}),
+        ('Date',            {'fields': ['date']}),
+        ('Price',           {'fields': ['price']}),
+        ('Serving_Type',    {'fields': ['serving_type']}),
+        ('Rating',          {'fields': ['rating']}),
+        ('Description',     {'fields': ['description']}),
+    ]
+
+admin.site.register(Beer, BeerAdmin)
--- beers/models.py
+++ beers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Beer(models.Model):
+    name = models.CharField(max_length=250)
+    brewer = models.CharField(max_length=250)
+    date = models.DateTimeField(default=datetime.now)
+    price = models.FloatField()
+    serving_type = models.CharField(max_length=250)
+    rating = models.CharField(max_length=250)
+    description = models.TextField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
