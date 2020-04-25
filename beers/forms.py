0754fcc9052e170f89e1151ae2b2620c05d6955a
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2020-04-25 20:53:08 +0530

    Modified Add Beer form
    - Rating and Serving time fields changed to ChoiceFields
    - base.html: Added fontawesome icons and now user can only
    add beers after logging in

--- beers/forms.py
+++ beers/forms.py
@@ -3,6 +3,12 @@ from django import forms
 from .models import Beer
 
 class AddBeerForm(forms.ModelForm):
+    CHOICES = (
+        ("1", "Bottle"),
+        ("2", "Draft"),
+    )
+    serving_type = forms.ChoiceField(choices=CHOICES)
+    rating = forms.ChoiceField(choices=[(x, x) for x in range(1, 6)])
 
     class Meta:
         model = Beer
