36c116ad39d23842b97c910c8443cc7edc29b633
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2020-04-30 04:55:59 +0530

    Add Flavor Attribute wheel
    Modified Beer model to add flavors
    Using Chart.js to plot the pie chart

--- beers/admin.py
+++ beers/admin.py
@@ -11,6 +11,18 @@ class BeerAdmin(admin.ModelAdmin):
         ('Serving_Type',    {'fields': ['serving_type']}),
         ('Rating',          {'fields': ['rating']}),
         ('Description',     {'fields': ['description']}),
+        ('Alcohol',         {'fields': ['alcohol']}),
+        ('Hoppy',           {'fields': ['hoppy']}),
+        ('Floral',          {'fields': ['floral']}),
+        ('Herbal',          {'fields': ['herbal']}),
+        ('Spicy',           {'fields': ['spicy']}),
+        ('Malty',           {'fields': ['malty']}),
+        ('Burnt',           {'fields': ['burnt']}),
+        ('Sweet',           {'fields': ['sweet']}),
+        ('Sour',            {'fields': ['sour']}),
+        ('Bitter',          {'fields': ['bitter']}),
+        ('Dry',             {'fields': ['dry']}),
+        ('Linger',          {'fields': ['linger']}),
     ]
 
 admin.site.register(Beer, BeerAdmin)
--- beers/models.py
+++ beers/models.py
@@ -11,5 +11,23 @@ class Beer(models.Model):
     rating = models.CharField(max_length=250)
     description = models.TextField(max_length=500, null=True)
 
+    alcohol = models.FloatField(default=5)
+    hoppy = models.FloatField(default=20)
+    floral = models.FloatField(default=10)
+    herbal= models.FloatField(default=5)
+    spicy = models.FloatField(default=10)
+    malty = models.FloatField(default=5)
+    burnt = models.FloatField(default=5)
+    sweet = models.FloatField(default=10)
+    sour = models.FloatField(default=5)
+    bitter = models.FloatField(default=10)
+    dry = models.FloatField(default=10)
+    linger = models.FloatField(default=5)
+
     def __str__(self):
         return self.name
+
+    def flavors(self):
+        return [self.alcohol, self.hoppy, self.floral, self.herbal, self.spicy,
+                self.malty, self.burnt, self.sweet, self.sour, self.bitter,
+                self.dry, self.linger]
